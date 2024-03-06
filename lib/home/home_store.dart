8236fea14f879ee1cc522a04fa9fd4e2fe23b5d8
Felipe <felipesf111@gmail.com>
2024-03-06 14:07:27 -0300

    feat: home page, store and infra

---
+++ lib/home/home_store.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
+
+import 'package:ws_car/home/infra/car_model.dart';
+import 'package:ws_car/home/infra/home_repository.dart';
+
+part 'home_store.g.dart';
+
+enum HomeState {
+  initial,
+  loading,
+  success,
+  failed,
+}
+
+class HomeStore = _HomeStoreBase with _$HomeStore;
+
+abstract class _HomeStoreBase with Store {
+  final HomeRepositoryImpl repository;
+
+  _HomeStoreBase(
+    this.repository,
+  );
+
+  @readonly
+  var _state = HomeState.initial;
+
+  @observable
+  List<CarModel>? cars;
+
+  @action
+  Future<void> getCars() async {
+    _state = HomeState.loading;
+    try {
+      cars = await repository.getCars();
+      _state = HomeState.success;
+    } catch (e) {
+      _state = HomeState.failed;
+      rethrow;
+    }
+  }
+}
---
+++ lib/home/infra/home_repository.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:ws_car/home/infra/car_model.dart';
+
+abstract class HomeRepository {
+  Future<List<CarModel>> getCars();
+}
+
+class HomeRepositoryImpl implements HomeRepository {
+  final dio = Dio();
+
+  @override
+  Future<List<CarModel>> getCars() async {
+    try {
+      final response = await dio.get("https://wswork.com.br/cars.json");
+      return response.data["cars"].map<CarModel>((c) {
+        return CarModel.fromJson(c);
+      }).toList();
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
