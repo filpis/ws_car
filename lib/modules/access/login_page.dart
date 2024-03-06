b0e8102751fc232a35f5e8929f0cd08e6448f29c
Felipe <felipesf111@gmail.com>
2024-03-06 14:08:54 -0300

    feat: login page

---
+++ lib/modules/access/login_page.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:ws_car/core/utils/constants.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController user = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: const Color(0xFF160e3b),
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                "${Assets.images}/ws_logo.png",
+              ),
+            ),
+            const SizedBox(
+              height: 400,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              width: MediaQuery.of(context).size.width,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: const Color(0xFFA4A4A4),
+                  width: 0.5,
+                ),
+              ),
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SvgPicture.asset(
+                    "${Assets.icons}/user_icon.svg",
+                    fit: BoxFit.fill,
+                    width: 15,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: user,
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        labelText: "Usuário",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: const Color(0xFFA4A4A4),
+                  width: 0.5,
+                ),
+              ),
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SvgPicture.asset(
+                    "${Assets.icons}/key_icon.svg",
+                    fit: BoxFit.fill,
+                    width: 15,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: password,
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        labelText: "Senha",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextButton(
+              onPressed: () {
+                Modular.to.pushNamed("home");
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xFF0c062b),
+                  border: Border.all(width: 1, color: Colors.white),
+                ),
+                width: MediaQuery.of(context).size.width,
+                height: 40,
+                child: const Center(
+                  child: Text(
+                    "Entrar",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 30,
+                  height: 0.5,
+                  color: Colors.white,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                const Text(
+                  "ou",
+                  style: TextStyle(color: Colors.white, fontSize: 14),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Container(
+                  width: 30,
+                  height: 0.5,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xFF0c062b),
+                  border: Border.all(width: 1, color: Colors.white),
+                ),
+                width: MediaQuery.of(context).size.width,
+                height: 40,
+                child: const Center(
+                  child: Text(
+                    "Cadastro",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
