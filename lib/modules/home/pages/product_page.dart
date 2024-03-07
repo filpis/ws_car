a0fe80dfd1a0ce0bfd43c4e04d2093f787d1e96b
Felipe <felipesf111@gmail.com>
2024-03-07 17:43:11 -0300

    feat: add more data on product page

--- lib/modules/home/pages/product_page.dart
+++ lib/modules/home/pages/product_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:ws_car/modules/home/infra/car_model.dart';
 
 class ProductPage extends StatefulWidget {
@@ -7,16 +9,169 @@ class ProductPage extends StatefulWidget {
   @override
   State<ProductPage> createState() => _ProductPageState();
 }
+//  {
 
+//          "id": 1,
+//          "timestamp_cadastro" : 1696539488,
+//          "modelo_id" : 18,
+//          "ano": 2015,
+//          "combustivel" : "FLEX",
+//          "num_portas" : 4,
+//          "cor": "BEGE",
+//          "nome_modelo" : "ONIX PLUS",
+//          "valor" : 50.000
+//         },
 class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {
+            Modular.to.pop();
+          },
+          child: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: const Color(0xFF160e3b),
+      ),
       body: Container(
-        child: Center(
-          child: Text(widget.car.cor ?? ""),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: const Offset(0, 1),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Image.network(
+                "https://www.tudodesenhos.com/uploads/images/2939/carro-de-4-portas.jpg",
+                height: 250,
+              ),
+            ),
+            Row(
+              children: [
+                const Text(
+                  "Modelo: ",
+                  style: TextStyle(fontWeight: FontWeight.w200, fontSize: 18),
+                ),
+                Text(
+                  widget.car.nomeModelo ?? "",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                const Text(
+                  "Combustível: ",
+                  style: TextStyle(fontWeight: FontWeight.w200, fontSize: 18),
+                ),
+                Text(
+                  widget.car.combustivel ?? "",
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                const Text(
+                  "Quantidade de portas: ",
+                  style: TextStyle(fontWeight: FontWeight.w200, fontSize: 18),
+                ),
+                const SizedBox(
+                  width: 2,
+                ),
+                Text(
+                  widget.car.numPortas.toString(),
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Cor: ${widget.car.cor}",
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                const Text(
+                  "Valor: ",
+                  style: TextStyle(fontWeight: FontWeight.w200, fontSize: 18),
+                ),
+                Text(
+                  formatarValorReal(widget.car.valor),
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            const Spacer(),
+            Observer(builder: (context) {
+              return TextButton(
+                onPressed: () {},
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFF0c062b),
+                    border: Border.all(width: 1, color: Colors.white),
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  child: const Center(
+                    child: Text(
+                      "Quero garantir!",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              );
+            }),
+          ],
         ),
       ),
     );
+  }
+}
+
+String formatarValorReal(double? valor) {
+  if (valor != null) {
+    String valorString = valor.toStringAsFixed(3);
+
+    return 'R\$ $valorString,00';
+  } else {
+    return "";
   }
 }
